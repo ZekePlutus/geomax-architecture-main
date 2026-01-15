10a7f76f5e9759b9159778abc026f226d47dc080
ZerroukZakaria <zerrouk.zakaria2002@gmail.com>
2026-01-15 15:16:37 +0100

    fix(datatable): remove unnecessary whitespace in drag-and-drop event handlers for improved readability

--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,32 @@ require __DIR__ . '/locale.php';
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Showcase Page
+Route::get('/components', function () {
+    return view('core-ui.components.showcase');
+})->name('components.showcase');
+
 // Component Examples
 Route::prefix('examples')->name('examples')->group(function () {
     Route::get('/', function () {
         return view('examples.index');
     });
-    
+
     // Form Components
     Route::get('/form/input', function () {
         return view('examples.form.input');
     })->name('.form.input');
-    
+
     Route::get('/form/range', function () {
         return view('examples.form.range');
     })->name('.form.range');
-    
+
     // General Components
     Route::get('/drawer', function () {
         return view('examples.drawer');
     })->name('.drawer');
-    
+
     Route::get('/datatable', function () {
         return view('examples.datatable');
     })->name('.datatable');
